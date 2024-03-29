3e1480c4a29f91753b41330e4011899e9c0acc40
Lucas <90059655+Lucas-ossli@users.noreply.github.com>
2024-03-29 09:14:47 -0300

    criação da aplicação mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CriptografiaDeImagem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
